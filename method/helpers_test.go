0f7ccda8d4cf6222706df6b0ef964e318d18406a
Adam Rothman <rothman.adam@gmail.com>
2020-08-27 15:17:57 -0700

    Improve handling of AWS regions other than us-east-1
    These changes are required in order to use this tool in regions outside of the
    standard AWS partition (e.g. aws-us-gov or aws-cn):
    
    * Use the AWS SDK to infer the correct S3 hostname given the configured region
    * Set the configured region on the base AWS session

---
+++ method/helpers_test.go
@@ -0,0 +1,63 @@
+package method
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestS3EndpointURL(t *testing.T) {
+	specs := map[string]struct {
+		expectedURL *url.URL
+		expectError bool
+	}{
+		"us-east-1": {
+			&url.URL{
+				Scheme: "https",
+				Host:   "s3.amazonaws.com",
+			},
+			false,
+		},
+		"us-west-2": {
+			&url.URL{
+				Scheme: "https",
+				Host:   "s3.us-west-2.amazonaws.com",
+			},
+			false,
+		},
+		"us-gov-east-1": {
+			&url.URL{
+				Scheme: "https",
+				Host:   "s3.us-gov-east-1.amazonaws.com",
+			},
+			false,
+		},
+		"cn-north-1": {
+			&url.URL{
+				Scheme: "https",
+				Host:   "s3.cn-north-1.amazonaws.com.cn",
+			},
+			false,
+		},
+		"outer-space-0": {
+			nil,
+			true,
+		},
+	}
+
+	for region, spec := range specs {
+		t.Run(region, func(t *testing.T) {
+			u, err := s3EndpointURL(region)
+			if err != nil && !spec.expectError {
+				t.Errorf("expected s3EndpointURL(%#v) not to return an error but got %#v", region, err)
+			} else if err == nil && spec.expectError {
+				t.Errorf("expected s3EndpointURL(%#v) to return an error but got none", region)
+			}
+
+			if diff := cmp.Diff(spec.expectedURL, u); diff != "" {
+				t.Errorf("s3EndpointURL(%#v) mismatch (-want +got):\n%s", region, diff)
+			}
+		})
+	}
+}

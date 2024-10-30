6890e21f06650d48f0033b3e430f78e92f8db91f
Adam Duke <adam.v.duke@gmail.com>
2024-10-30 11:36:12 -0400

    add gochecknoglobals linter and associated fixes

--- method/method_test.go
+++ method/method_test.go
@@ -110,35 +110,35 @@ type locTest struct {
 	accessKeySecret string
 }
 
-var locTests = []locTest{
-	{
-		"s3://fake-access-key-id:fake-access-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
-		"fake-access-key-id",
-		"fake-access-key-secret",
-	},
-	{
-		"s3://fake-ac/cess-key-id:fake-ac/cess-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
-		"fake-ac/cess-key-id",
-		"fake-ac/cess-key-secret", // secret contains a forward slash
-	},
-	{
-		"s3://fake-ac%2Fcess-key-id:fake-ac%2Fcess-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
-		"fake-ac/cess-key-id",     // access key contains a forward slash that was encoded as %2F in the original url
-		"fake-ac/cess-key-secret", // secret contains a forward slash that was encoded as %2F in the original url
-	},
-	{
-		"s3://fake-access-key-id:@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
-		"fake-access-key-id",
-		"", // secret is blank
-	},
-	{
-		"s3://:fake-access-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
-		"", // access key is blank
-		"fake-access-key-secret",
-	},
-}
-
 func TestCreateLocation(t *testing.T) {
+	locTests := []locTest{
+		{
+			"s3://fake-access-key-id:fake-access-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
+			"fake-access-key-id",
+			"fake-access-key-secret",
+		},
+		{
+			"s3://fake-ac/cess-key-id:fake-ac/cess-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
+			"fake-ac/cess-key-id",
+			"fake-ac/cess-key-secret", // secret contains a forward slash
+		},
+		{
+			"s3://fake-ac%2Fcess-key-id:fake-ac%2Fcess-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
+			"fake-ac/cess-key-id",     // access key contains a forward slash that was encoded as %2F in the original url
+			"fake-ac/cess-key-secret", // secret contains a forward slash that was encoded as %2F in the original url
+		},
+		{
+			"s3://fake-access-key-id:@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
+			"fake-access-key-id",
+			"", // secret is blank
+		},
+		{
+			"s3://:fake-access-key-secret@s3.amazonaws.com/apt-repo-bucket/apt/generic/python-bernhard_0.2.3-1_all.deb",
+			"", // access key is blank
+			"fake-access-key-secret",
+		},
+	}
+
 	for _, lt := range locTests {
 		l, err := newLocation(lt.url, "s3.amazonaws.com")
 		if err != nil {
